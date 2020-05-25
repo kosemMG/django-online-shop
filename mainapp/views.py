100dbffbc2b8921bd76c972e08bee5d0fe10c605
Moshe Gelberg <glbrgm@gmail.com>
2020-05-25 14:42:40 +0300

    Mingor bugs fix

--- mainapp/views.py
+++ mainapp/views.py
@@ -63,25 +63,63 @@ def product(request):
     title = 'product - ' + SHOP_NAME
     body_class = 'products'
     products = Product.objects.filter(category__pk=2).order_by('price')
+
+    cart = []
+    if request.user.is_authenticated:
+        cart = Cart.objects.filter(user=request.user)
+
+    cart_products = []
+    for cart_product in cart:
+        product = Product.objects.get(pk=cart_product.product_id)
+        cart_products.append({
+            'product': product,
+            'number': cart_product.amount,
+            'subtotal': product.price * cart_product.amount
+        })
+
+    cart_amount = len(products)
+
     content = {
         'title': title,
         'body_class': body_class,
         'menu': menu,
-        'products': products
+        'cart_amount': cart_amount,
+        'products': products,
+        'cart_products': cart_products,
+        'cart': cart
     }
     return render(request, 'mainapp/product.html', content)
 
 
-def single_page(request, pk=None):
+def single_page(request, pk):
     title = 'single page - ' + SHOP_NAME
     body_class = 'single-page'
-    product = get_object_or_404(Product, pk=pk)
+    single_product = get_object_or_404(Product, pk=pk)
     page_products = Product.objects.filter(category__pk=3).order_by('price')
+
+    cart = []
+    if request.user.is_authenticated:
+        cart = Cart.objects.filter(user=request.user)
+
+    products = []
+    for cart_product in cart:
+        product = Product.objects.get(pk=cart_product.product_id)
+        products.append({
+            'product': product,
+            'number': cart_product.amount,
+            'subtotal': product.price * cart_product.amount
+        })
+
+    cart_amount = len(products)
+
     content = {
         'title': title,
         'body_class': body_class,
         'menu': menu,
-        'product': product,
-        'products': page_products
+        'cart_amount': cart_amount,
+        'product': single_product,
+        'products': page_products,
+        'cart_products': products,
+        'cart': cart
     }
     return render(request, 'mainapp/single-page.html', content)
